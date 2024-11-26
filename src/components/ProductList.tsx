034f635ae38d67218b5f9c086e9ea9e43e237511
bagusfaize <bagusfaize@gmail.com>
2024-11-26 21:49:52 +0700

    style: styling product list

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -15,7 +15,7 @@ export default function ProductList({
                 <div className="col-span-3 flex justify-center items-center">
                     <Image
                         src={product.images[0]}
-                        className="h-28 w-auto"
+                        className="h-28 w-auto object-contain"
                         alt={`product-${product.id}-images`}
                         height={50}
                         width={50}
@@ -24,7 +24,7 @@ export default function ProductList({
                 </div>
                 <div className="col-span-9">
                     <div className="text-slate-800 font-bold text-xl mb-2">{product.title}</div>
-                    <p className="text-slate-600 text-sm">{product.description}</p>
+                    <p className="text-slate-600 text-sm line-clamp-2">{product.description}</p>
                     <div className="flex items-center gap-1 my-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
