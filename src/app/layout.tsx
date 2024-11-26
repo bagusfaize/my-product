2e3b8dfee0233c19b51767d6eeea0188627175b0
bagusfaize <bagusfaize@gmail.com>
2024-11-26 21:41:50 +0700

    chore: setup api instance, setup react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import ReactQueryProvider from "@/providers/ReactQueryProvider";
+import Container from "@/components/Container";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +28,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-100`}
       >
-        {children}
+        <ReactQueryProvider>
+          <Container>
+            {children}
+          </Container>
+        </ReactQueryProvider>
       </body>
     </html>
   );
